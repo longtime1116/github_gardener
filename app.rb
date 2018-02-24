35c9fabee6cae6bce54430427e0a0a6798d60a72
longtime1116 <longtime1116@gmail.com>
2018-02-24 18:20:12 +0900

    add nokogiri usage

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra/reloader'
 require 'sinatra/json'
 require 'uri'
 require 'net/http'
+require 'nokogiri'
+#require 'pry'
 
 get '/' do
   'hello world'
@@ -12,5 +14,13 @@ get "/:name" do
   uri = URI.parse("https://github.com/users/#{params[:name]}/contributions")
   # この garden_svg は String なので、こいつを良い感じに parse したい
   garden_svg = Net::HTTP.get_response(uri).body
+  doc = Nokogiri::HTML.parse(garden_svg)
+  #binding.pry
+  # doc.css("rect").each
+  # doc.css("rect").first.attributes["data-date"].value
+  # doc.css("rect").first.attributes["data-count"].value
+
+  # p garden_svg
+  # return garden_svg
   return garden_svg
 end
