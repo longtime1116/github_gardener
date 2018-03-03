88f8ba786cc8d7c1c65708ff50c3a9f93d41358e
longtime1116 <longtime1116@gmail.com>
2018-03-03 10:29:28 +0900

    add Garden class

--- app.rb
+++ app.rb
@@ -11,16 +11,45 @@ get '/' do
 end
 
 get "/:name" do
-  uri = URI.parse("https://github.com/users/#{params[:name]}/contributions")
-  # この garden_svg は String なので、こいつを良い感じに parse したい
-  garden_svg = Net::HTTP.get_response(uri).body
-  doc = Nokogiri::HTML.parse(garden_svg)
-  #binding.pry
-  # doc.css("rect").each
-  # doc.css("rect").first.attributes["data-date"].value
-  # doc.css("rect").first.attributes["data-count"].value
+  @user_name = params[:name]
+  garden = Garden.new(@user_name)
+  @garden_svg = garden.garden_svg
+  @consecutive_days = garden.consecutive_days
+  @consecutive_total_contribs = garden.consecutive_total_contribs
+  @consecutive_average_contribs = garden.consecutive_average_contribs
 
-  # p garden_svg
-  # return garden_svg
-  return garden_svg
+  #binding.pry
+  erb  :index
+end
+
+
+class Garden
+  attr_reader :garden_svg, :consecutive_days, :consecutive_total_contribs, :consecutive_average_contribs
+
+  def initialize(user_name)
+    uri = URI.parse("https://github.com/users/#{user_name}/contributions")
+    @garden_svg = Net::HTTP.get_response(uri).body
+    doc = Nokogiri::HTML.parse(@garden_svg)
+
+    consecutive_days = 0
+    consecutive_total_contribs = 0
+
+    rects = doc.css("rect").reverse
+    rects[1..-1].each do |rect|
+      break if rect.attributes["data-count"].value.to_i == 0
+      consecutive_days += 1
+      consecutive_total_contribs += rect.attributes["data-count"].value.to_i
+    end
+
+    if rects[0].attributes["data-count"].value.to_i != 0
+      consecutive_days += 1
+      consecutive_total_contribs += rects[0].attributes["data-count"].value.to_i
+    end
+
+    @consecutive_days = consecutive_days
+    @consecutive_total_contribs = consecutive_total_contribs
+    @consecutive_average_contribs = (consecutive_total_contribs.to_f / consecutive_days).round(2)
+  end
+
+
 end
