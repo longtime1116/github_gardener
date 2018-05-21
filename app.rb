9e9d1747ce1bf80c8325a4a1a55ba79140117516
longtime1116 <longtime1116@gmail.com>
2018-05-21 09:57:34 +0900

    リファクタリング

--- app.rb
+++ app.rb
@@ -15,86 +15,96 @@ post '/redirect_to_garden' do
 end
 
 get "/:name" do
-  @user_name = params[:name]
-  garden = Garden.new(@user_name)
-  @garden_svg = garden.garden_svg
-  redirect "/" if @garden_svg.nil?
+  @garden = Garden.new(params[:name])
+  redirect "/" if @garden.has_no_owner?
 
-  @consecutive_days = garden.consecutive_days
-  @consecutive_total_contribs = garden.consecutive_total_contribs
-  @consecutive_average_contribs = garden.consecutive_average_contribs
-  @contributed_days_last_year = garden.contributed_days_last_year
-  @total_contribs_last_year = garden.total_contribs_last_year
-  @average_contribs_last_year = garden.average_contribs_last_year
-  @graph_data = garden.contribs_per_week
   erb  :garden
 end
 
 
 class Garden
-  attr_reader :garden_svg,
-              :consecutive_days,
-              :consecutive_total_contribs,
-              :consecutive_average_contribs,
-              :contributed_days_last_year,
-              :total_contribs_last_year,
-              :average_contribs_last_year,
-              :contribs_per_week
+  attr_reader :user_name, :garden_svg
 
   def initialize(user_name)
-    uri = URI.parse("https://github.com/users/#{user_name}/contributions")
-    @garden_svg = Net::HTTP.get_response(uri).body
-    if @garden_svg.to_s == "Not Found"
-      @garden_svg = nil
-      return
+    @user_name = user_name
+    @garden_svg = fetch_garden_svg
+    @rects = Nokogiri::HTML.parse(@garden_svg).css("rect")
+  end
+
+  def has_no_owner?
+    @garden_svg.nil?
+  end
+
+  def consecutive_days
+    rects = @rects.reverse
+    count = 0
+
+    rects[1..-1].each do |rect|
+      break if contribute_count_of(rect) == 0
+      count += 1
     end
 
-    doc = Nokogiri::HTML.parse(@garden_svg)
-    rects = doc.css("rect")
+    count += 1 if contribute_count_of(rects[0]) != 0
+    count
+  end
 
-    @consecutive_days, @consecutive_total_contribs, @consecutive_average_contribs = consecutive_stats(rects.reverse)
-    @contributed_days_last_year, @total_contribs_last_year, @average_contribs_last_year = last_year_stats(rects.reverse)
-    @contribs_per_week = contributions_per_week(rects)
+  def consecutive_total_contribs
+    rects = @rects.reverse
+    total = 0
+
+    rects[1..-1].each do |rect|
+      break if contribute_count_of(rect) == 0
+      total += contribute_count_of(rect)
+    end
+
+    total += contribute_count_of(rects[0]) if contribute_count_of(rects[0]) != 0
+    total
+  end
+
+  def consecutive_average_contribs
+    return 0.0 if consecutive_days == 0
+    (consecutive_total_contribs.to_f / consecutive_days).round(2)
+  end
+
+  def contributed_days_last_year
+    count = 0
+    @rects.each do |rect|
+      next if contribute_count_of(rect) == 0
+      count += 1
+    end
+    count
+  end
+
+  def total_contribs_last_year
+    total = 0
+
+    @rects.each do |rect|
+      next if contribute_count_of(rect) == 0
+      total += contribute_count_of(rect)
+    end
+    total
+  end
+
+  def average_contribs_last_year
+    return 0.0 if contributed_days_last_year == 0
+    (total_contribs_last_year / contributed_days_last_year).round(2)
+  end
+
+  def contribs_per_week
+    data = {}
+    @rects.each_slice(7) do |week_rects|
+      data[date_of(week_rects.first)] = week_rects.sum { |rect| contribute_count_of(rect) }
+    end
+    data
   end
 
   private
 
-  def consecutive_stats(rects)
-    consecutive_days = 0
-    consecutive_total_contribs = 0
-
-    rects[1..-1].each do |rect|
-      break if contribute_count_of(rect) == 0
-      consecutive_days += 1
-      consecutive_total_contribs += contribute_count_of(rect)
-    end
-    if contribute_count_of(rects[0]) != 0
-      consecutive_days += 1
-      consecutive_total_contribs += contribute_count_of(rects[0])
-    end
-    return [consecutive_days, consecutive_total_contribs, 0.0] if consecutive_days == 0
-    [consecutive_days, consecutive_total_contribs, (consecutive_total_contribs.to_f / consecutive_days).round(2)]
-  end
-
-  def last_year_stats(rects)
-    contributed_days_last_year = 0
-    total_contribs_last_year = 0
-
-    rects.each do |rect|
-      next if contribute_count_of(rect) == 0
-      contributed_days_last_year += 1
-      total_contribs_last_year += contribute_count_of(rect)
-    end
-    return [contributed_days_last_year, total_contribs_last_year, 0.0] if contributed_days_last_year == 0
-    [contributed_days_last_year, total_contribs_last_year, (total_contribs_last_year / contributed_days_last_year).round(2)]
-  end
-
-  def contributions_per_week(rects)
-    data = {}
-    rects.each_slice(7) do |week_rects|
-      data[date_of(week_rects.first)] = week_rects.sum { |rect| contribute_count_of(rect) }
-    end
-    data
+  def fetch_garden_svg
+    uri = URI.parse("https://github.com/users/#{user_name}/contributions")
+    garden_svg = Net::HTTP.get_response(uri).body
+    return nil if garden_svg.to_s == "Not Found"
+    garden_svg
   end
 
   def date_of(rect)
